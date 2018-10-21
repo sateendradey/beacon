a73d150f1af2da4c1564ce3476bfd135b152bcbd
Sateendra Dey <37790019+sateendradey@users.noreply.github.com>
2018-10-21 04:49:54 -0500

    Cors

--- app.js
+++ app.js
@@ -2,7 +2,13 @@ const express = require('express')
 const app = express()
 var bodyParser = require('body-parser');
 const port = process.env.PORT || 3000
-
+const cors = require('cors');//
+const corsOptions = {
+  origin: '*',
+  methods: 'GET,PUT,POST,DELETE',
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
+};//
 
 // database integration
 const dburi = "mongodb+srv://sateendradey:WordPass1990!@cluster0-wgoht.mongodb.net/test?retryWrites=true";
@@ -16,7 +22,7 @@ app.use(bodyParser.urlencoded({
     extended: false
 }));
 app.use(express.json());
-
+app.use(cors(corsOptions));//
 app.get('/', (req, res) => res.send('Hello World!'))
 // beacon end points
 
